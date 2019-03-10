4c6b7378c423f1062d7de685d6e705aed77db9b2
domi22 <491431567@qq.com>
2019-03-10 22:47:16 +0800

    updata pom

---
+++ spring-cloud-gateway/src/main/java/spring/cloud/gateway/SpringCloudGatewayApp.java
@@ -0,0 +1,21 @@
+package spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class SpringCloudGatewayApp extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApp.class);
+    }
+
+    /**
+     * 加载到外部容器启动
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringCloudGatewayApp.class);
+    }
+}
