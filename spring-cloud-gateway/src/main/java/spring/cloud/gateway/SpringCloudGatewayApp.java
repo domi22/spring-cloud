bb09c7acf57e8300af151e5c5a988de20e4762e1
domi22 <491431567@qq.com>
2019-03-11 01:02:01 +0800

    updata pom

--- spring-cloud-gateway/src/main/java/spring/cloud/gateway/SpringCloudGatewayApp.java
+++ spring-cloud-gateway/src/main/java/spring/cloud/gateway/SpringCloudGatewayApp.java
@@ -1,8 +1,13 @@
 package spring.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
 public class SpringCloudGatewayApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -11,6 +16,7 @@ public class SpringCloudGatewayApp extends SpringBootServletInitializer {
 
     /**
      * 加载到外部容器启动
+     *
      * @param application
      * @return
      */
