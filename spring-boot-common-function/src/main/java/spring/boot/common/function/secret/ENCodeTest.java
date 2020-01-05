bb3512a7f1c66a0e3d14f34377e43a6577f2b804
domi22 <491431567@qq.com>
2020-01-05 16:35:53 +0800

    MD5

--- spring-boot-common-function/src/main/java/spring/boot/common/function/secret/ENCodeTest.java
+++ spring-boot-common-function/src/main/java/spring/boot/common/function/secret/ENCodeTest.java
@@ -1,23 +1,34 @@
 package spring.boot.common.function.secret;
 
+import org.apache.commons.codec.CharEncoding;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.security.*;
 
 /**
  * 编码方式的测试类
  */
 public class ENCodeTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        /**
+         * 编码方式
+         */
+        testUrlEncode();
+        /**
+         * 摘要算法
+         */
+        testMD5();
+    }
 
-
+    private static void testUrlEncode() {
         String url = "taobao.comn?name=你好&age=27";
         try {
-            String encode = URLEncoder.encode(url,"UTF-8");
+            String encode = URLEncoder.encode(url,CharEncoding.UTF_8);
             System.out.println("encode>>>" + encode);
 
-            String decode = URLDecoder.decode(encode, "UTF-8");
+            String decode = URLDecoder.decode(encode, CharEncoding.UTF_8);
             System.out.println("decode>>>" + decode);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
@@ -26,13 +37,21 @@ public class ENCodeTest {
         encode>>>taobao.comn%3Fname%3D%E4%BD%A0%E5%A5%BD%26age%3D27
         decode>>>taobao.comn?name=你好&age=27
          */
+    }
 
+    private static void testMD5() {
+        try {
+            // SHA-1  MD5 SAH-256 SHA-512
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            String name = "helloword";
+            md.update(name.getBytes(CharEncoding.UTF_8));
+            byte[] digest = md.digest();
 
-
-
-
-
-
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
 
